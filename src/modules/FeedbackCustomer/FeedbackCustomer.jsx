ff2ea6c8a4734c9ca37ac9ab848bb80ebcb64945
Vlad Danilov <vladdanilov999@gmail.com>
2023-02-20 14:18:34 +0200

    update

--- src/modules/FeedbackCustomer/FeedbackCustomer.jsx
+++ src/modules/FeedbackCustomer/FeedbackCustomer.jsx
@@ -1,11 +1,59 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Statistic from './Statistic';
 import FeedbackOptions from './FeedbackOptions';
 import Section from './Section';
 import Notification from './Notification';
 
-// import css from './feedback-customer.module.css';
+const FeedbackCustomer = () => {
+  const [vote, setVote] = useState({ good: 0, neutral: 0, bad: 0 });
+  const { good, neutral, bad } = vote;
 
+  const totalVote = good + neutral + bad;
+
+  const countPositiveFeedbackPercentage = prevState => {
+    if (!totalVote) {
+      return 0;
+    }
+    const value = vote[prevState];
+    const votesPercentage = Number((value / totalVote) * 100).toFixed(2);
+
+    return votesPercentage;
+  };
+
+  const addVote = name => {
+    setVote(prevState => {
+      return { ...prevState, [name]: prevState[name] + 1 };
+    });
+  };
+
+  const arrayOptionBtn = Object.keys(vote);
+  const PositiveResultPercentage = countPositiveFeedbackPercentage('good');
+
+  return (
+    <>
+      <Section
+        tittle={'Please leave feedback'}
+        text={'The total number of people who passed the survey:'}
+        totalResultFeedback={totalVote}
+      >
+        <FeedbackOptions addVote={addVote} option={arrayOptionBtn} />
+        {totalVote ? (
+          <Statistic
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            PositiveResultPercentage={PositiveResultPercentage}
+          />
+        ) : (
+          <Notification tittle={'There is no feedback'} />
+        )}
+      </Section>
+    </>
+  );
+};
+
+export default FeedbackCustomer;
+/*
 class FeedbackCustomer extends Component {
   state = {
     good: 0,
@@ -69,3 +117,4 @@ class FeedbackCustomer extends Component {
 }
 
 export default FeedbackCustomer;
+*/
